424ea01dcf702d01c9a96e1f2d1fb4c227e533f9
Lukasrss <lucas.aprigio@hotmail.com>
2023-02-21 18:27:14 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,30 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //int numero = 5;
+
+        /*numero = - numero;
+        System.out.println(numero);
+        numero = - numero;
+        System.out.println(numero);*/
+
+        //numero++ = numero = numero + 1;
+        /*numero++;
+        System.out.println(numero);
+        System.out.println(-- numero);*/
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /*String resultado = "";
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";*/
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        
+        System.out.println(resultado);
+    }
+}
