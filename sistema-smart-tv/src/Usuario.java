ceda61295f90a9e89e36b5724c6b677b250f1660
Lukasrss <lucas.aprigio@hotmail.com>
2023-02-21 21:00:05 -0300

    exercicio Smart TV

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual? " + smartTv.canal);
+        System.out.println("Volume Atual? " + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+            
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        
+    }
+}
